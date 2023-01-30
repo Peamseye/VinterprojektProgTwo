637a1384a55098f840519274569809b358b9fb8c
Peamseye <aispotato587@gmail.com>
2023-01-30 12:52:06 +0100

    lhiohoi

--- VinterprojektProgTwo/Enemy.cs
+++ VinterprojektProgTwo/Enemy.cs
@@ -4,9 +4,13 @@ public class Enemy
 
 private int EHp = 100;
 
-public int SetEHp(int eh) {
+public void SetEHp(int eh) {
 EHp = eh;
 if (EHp < 0) EHp = 0;
+}
+
+public int GetHp() {
+    return EHp;
 }
 
 }
