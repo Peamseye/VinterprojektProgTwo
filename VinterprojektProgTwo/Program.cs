e1910d5321d36715d6b7d73086a00a95abd51f35
Peamseye <aispotato587@gmail.com>
2023-01-30 10:21:52 +0100

    test30123

--- VinterprojektProgTwo/Program.cs
+++ VinterprojektProgTwo/Program.cs
@@ -4,8 +4,8 @@ Hero h1 = new Hero();
 Enemy e1 = new Enemy();
 
 
-Console.ReadLine();
 
+Console.WriteLine("What's the name of your hero?");
 h1.Name = Console.ReadLine();
 Console.WriteLine($"Your hero's name is {h1.Name}.");
 
