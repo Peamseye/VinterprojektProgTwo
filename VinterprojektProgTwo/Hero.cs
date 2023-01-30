b4708e48ef6c8f8ca5ee5c4038527a8ba2991dfc
Peamseye <aispotato587@gmail.com>
2023-01-30 09:43:58 +0100

    Speed

--- VinterprojektProgTwo/Hero.cs
+++ VinterprojektProgTwo/Hero.cs
@@ -1,14 +1,35 @@
 
 public class Hero 
 {
-public string name = "hero";
-private int hp = 100;
+public string Name = "hero";
+private int Hp = 100;
 
-public int speed = 1;
+public int Speed = 50;
 
+public int SpeedChance(int s) {
+    Speed = s;
+    Random generator = new Random();
+    int Chance = generator.Next(1, 100);
 
-public int returnHP() {
-return hp;
+    if (s > Chance) {
+        s = 1;
+    }
+    else {
+        s = 0;
+    }
+    
+    return s;
 }
+
+public void SetHp(int h) {
+    Hp = h;
+    if (Hp < 0) Hp = 0;
+}
+
+
+public int GetHp() {
+return Hp;
+}
+
 
 }
