f7e0bd45dc4a9b3a374b9731f8357862f7ad5153
Peamseye <aispotato587@gmail.com>
2023-01-30 10:24:25 +0100

    weapon

--- VinterprojektProgTwo/Program.cs
+++ VinterprojektProgTwo/Program.cs
@@ -10,3 +10,10 @@ h1.Name = Console.ReadLine();
 Console.WriteLine($"Your hero's name is {h1.Name}.");
 
 Console.ReadLine();
+
+Console.WriteLine("Please choose your weapon.");
+Console.WriteLine("1. Sword     2. Axe     3. Spear     4. Crossbow");
+
+
+Console.ReadLine();
+
